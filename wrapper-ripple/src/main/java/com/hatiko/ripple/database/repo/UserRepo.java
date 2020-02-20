5136179855a7f3c058791dfeb915c703ae2a50e4
Hat1ko <pasha1apple@gmail.com>
2020-02-20 17:03:55 +0200

    CrudRepository -> JpaRepository

--- wrapper-ripple/src/main/java/com/hatiko/ripple/database/repo/UserRepo.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/database/repo/UserRepo.java
@@ -1,9 +1,9 @@
 package com.hatiko.ripple.database.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hatiko.ripple.database.dto.UserEntity;
 
-public interface UserRepo extends CrudRepository<UserEntity, Long> {
+public interface UserRepo extends JpaRepository<UserEntity, Long> {
 
 }
