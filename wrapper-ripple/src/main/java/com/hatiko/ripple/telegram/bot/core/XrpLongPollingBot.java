551883806654234904d50da3e8f5baa8cf9c799d
Hat1ko <pasha1apple@gmail.com>
2020-03-25 18:30:51 +0200

    created structure for core + created skeleton for LongPolling

---
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/XrpLongPollingBot.java
@@ -0,0 +1,25 @@
+package com.hatiko.ripple.telegram.bot.core;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class XrpLongPollingBot extends TelegramLongPollingBot{
+
+	@Override
+	public void onUpdateReceived(Update update) {
+
+	}
+
+	@Override
+	public String getBotUsername() {
+
+		return null;
+	}
+
+	@Override
+	public String getBotToken() {
+		
+		return null;
+	}
+
+}
