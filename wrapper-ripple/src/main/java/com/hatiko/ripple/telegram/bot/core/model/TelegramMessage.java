1a56e8abb6adb714b79d70772ecbbe7bc9f25762
Hat1ko <pasha1apple@gmail.com>
2020-03-25 19:08:56 +0200

    created TelegramMessage model

---
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/model/TelegramMessage.java
@@ -0,0 +1,35 @@
+package com.hatiko.ripple.telegram.bot.core.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(of = {"id"})
+@AllArgsConstructor
+@NoArgsConstructor
+public class TelegramMessage {
+
+	@Id
+	private Integer id;
+	private LocalDateTime creationTime;
+	private String text;
+	
+	@ManyToOne
+	private TelegramUser from;
+	
+	@ManyToOne
+	private TelegramChat chat;
+}
