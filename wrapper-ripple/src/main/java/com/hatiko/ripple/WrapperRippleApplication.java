719cce3f3f58b0a6b417c8a1cfbb0501985dca67
Hat1ko <pasha1apple@gmail.com>
2020-03-25 18:01:31 +0200

    added telegram bot starter and injected initializer to main method

--- wrapper-ripple/src/main/java/com/hatiko/ripple/WrapperRippleApplication.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/WrapperRippleApplication.java
@@ -2,12 +2,17 @@ package com.hatiko.ripple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 //@EnableScheduling
 @SpringBootApplication
 public class WrapperRippleApplication {
     public static void main(String[] args) {
+    	
+    	//init Telegram bot
+    	ApiContextInitializer.init();
+    	
+    	//spring framework start
         SpringApplication.run(WrapperRippleApplication.class, args);
     }
 }
