d7974286470b04b862544355d10517ec2755f501
Hat1ko <pasha1apple@gmail.com>
2020-03-26 12:38:49 +0200

    added CommandsProperties

---
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/properties/CommandsProperties.java
@@ -0,0 +1,24 @@
+package com.hatiko.ripple.telegram.bot.core.properties;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class CommandsProperties {
+
+	private String hello;
+	private String getBalance;
+	private String generateMemo;
+	private String getTransactionInfo;
+	private String withdraw;
+}
