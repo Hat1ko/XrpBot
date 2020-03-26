36db78552674d4c41d8d284b5c48a987e42be305
Hat1ko <pasha1apple@gmail.com>
2020-03-26 12:26:20 +0200

    created message handler interface

---
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/handler/TelegramMessageHandler.java
@@ -0,0 +1,7 @@
+package com.hatiko.ripple.telegram.bot.core.handler;
+
+import com.hatiko.ripple.telegram.bot.core.model.TelegramUpdate;
+
+public interface TelegramMessageHandler {
+	void handle(TelegramUpdate telegramUpdate);
+}
