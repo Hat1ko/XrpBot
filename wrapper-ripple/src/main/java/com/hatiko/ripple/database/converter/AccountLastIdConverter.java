20634be34735bf752dc6569aea52d288d47cad1e
Hat1ko <pasha1apple@gmail.com>
2020-03-26 18:57:15 +0200

    refact

--- wrapper-ripple/src/main/java/com/hatiko/ripple/database/converter/AccountLastIdConverter.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/database/converter/AccountLastIdConverter.java
@@ -6,8 +6,11 @@ import com.hatiko.ripple.database.model.AccountLastIdEntity;
 public final class AccountLastIdConverter {
 
 	public static AccountLastIdEntity toEntity(AccountLastIdDTO accountLastIdDTO) {
-		return AccountLastIdEntity.builder().publicKey(accountLastIdDTO.getPublicKey())
-				.lastId(accountLastIdDTO.getLastId()).lastLedger(accountLastIdDTO.getLastLedger()).build();
+		return AccountLastIdEntity.builder()
+				.publicKey(accountLastIdDTO.getPublicKey())
+				.lastId(accountLastIdDTO.getLastId())
+				.lastLedger(accountLastIdDTO.getLastLedger())
+				.build();
 	}
 
 	public static AccountLastIdDTO toDTO(AccountLastIdEntity accountLastIdEntity) {
