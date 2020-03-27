5d91d8bda01dbc154a9b809c4f2039f1377cf29b
Hat1ko <pasha1apple@gmail.com>
2020-03-27 23:03:07 +0200

    made that method PostConstruct

--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/service/KeyboardPreparatorImpl.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/service/KeyboardPreparatorImpl.java
@@ -2,7 +2,8 @@ package com.hatiko.ripple.telegram.bot.core.service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
@@ -39,42 +40,38 @@ public class KeyboardPreparatorImpl implements KeyboardPreparator {
 		return null;
 	}
 
-	private Map<String, KeyboardButton> getButtons() {
+	@PostConstruct
+	private void createButtons() {
 
-		if (Objects.isNull(buttons)) {
+		buttons = new HashMap<String, KeyboardButton>();
 
-			buttons = new HashMap<String, KeyboardButton>();
+		KeyboardButton helloButton = new KeyboardButton(actionProperties.getCommand().getHello());
+		helloButton.setText(actionProperties.getButton().getHello());
+		buttons.put("hello", helloButton);
 
-			KeyboardButton helloButton = new KeyboardButton(actionProperties.getCommand().getHello());
-			helloButton.setText(actionProperties.getButton().getHello());
-			buttons.put("hello", helloButton);
+		KeyboardButton helpButton = new KeyboardButton(actionProperties.getCommand().getHelp());
+		helpButton.setText(actionProperties.getButton().getHelp());
+		buttons.put("help", helpButton);
 
-			KeyboardButton helpButton = new KeyboardButton(actionProperties.getCommand().getHelp());
-			helpButton.setText(actionProperties.getButton().getHelp());
-			buttons.put("help", helpButton);
+		KeyboardButton logInButton = new KeyboardButton(actionProperties.getCommand().getLogIn());
+		helpButton.setText(actionProperties.getButton().getLogIn());
+		buttons.put("logIn", logInButton);
 
-			KeyboardButton logInButton = new KeyboardButton(actionProperties.getCommand().getLogIn());
-			helpButton.setText(actionProperties.getButton().getLogIn());
-			buttons.put("logIn", logInButton);
+		KeyboardButton getBalanceButton = new KeyboardButton(actionProperties.getCommand().getGetBalance());
+		getBalanceButton.setText(actionProperties.getButton().getGetBalance());
+		buttons.put("getBalance", getBalanceButton);
 
-			KeyboardButton getBalanceButton = new KeyboardButton(actionProperties.getCommand().getGetBalance());
-			getBalanceButton.setText(actionProperties.getButton().getGetBalance());
-			buttons.put("getBalance", getBalanceButton);
+		KeyboardButton generateMemoButton = new KeyboardButton(actionProperties.getCommand().getGenerateMemo());
+		generateMemoButton.setText(actionProperties.getButton().getGenerateMemo());
+		buttons.put("generateBalance", generateMemoButton);
 
-			KeyboardButton generateMemoButton = new KeyboardButton(actionProperties.getCommand().getGenerateMemo());
-			generateMemoButton.setText(actionProperties.getButton().getGenerateMemo());
-			buttons.put("generateBalance", generateMemoButton);
+		KeyboardButton getTransactionInfoButton = new KeyboardButton(
+				actionProperties.getCommand().getGetTransactionInfo());
+		getTransactionInfoButton.setText(actionProperties.getButton().getGetTransactionInfo());
+		buttons.put("getTransactionInfo", getTransactionInfoButton);
 
-			KeyboardButton getTransactionInfoButton = new KeyboardButton(
-					actionProperties.getCommand().getGetTransactionInfo());
-			getTransactionInfoButton.setText(actionProperties.getButton().getGetTransactionInfo());
-			buttons.put("getTransactionInfo", getTransactionInfoButton);
-
-			KeyboardButton withdrawButton = new KeyboardButton(actionProperties.getCommand().getWithdraw());
-			withdrawButton.setText(actionProperties.getButton().getWithdraw());
-			buttons.put("withdraw", withdrawButton);
-		}
-
-		return buttons;
+		KeyboardButton withdrawButton = new KeyboardButton(actionProperties.getCommand().getWithdraw());
+		withdrawButton.setText(actionProperties.getButton().getWithdraw());
+		buttons.put("withdraw", withdrawButton);
 	}
 }
