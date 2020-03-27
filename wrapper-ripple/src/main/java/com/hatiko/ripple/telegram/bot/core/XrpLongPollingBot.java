01c00ae9bfefa9433a604cb2380c2e3c00e8781e
Hat1ko <pasha1apple@gmail.com>
2020-03-27 13:27:10 +0200

    overriden all TelegramLongPollingBot methods

--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/XrpLongPollingBot.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/XrpLongPollingBot.java
@@ -1,10 +1,16 @@
 package com.hatiko.ripple.telegram.bot.core;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import com.hatiko.ripple.telegram.bot.core.handler.TelegramMessageHandler;
+import com.hatiko.ripple.telegram.bot.core.model.TelegramUpdate;
 import com.hatiko.ripple.telegram.bot.core.properties.XrpBotProperties;
+import com.hatiko.ripple.telegram.bot.core.service.TelegramUpdateService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,24 +18,27 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class XrpLongPollingBot extends TelegramLongPollingBot{
+public class XrpLongPollingBot extends TelegramLongPollingBot {
 
+	@Lazy
+	private final List<TelegramMessageHandler> telegramMessageHandlers;
+
+	private final TelegramUpdateService telegramUpdateService;
 	private final XrpBotProperties xrpBotProperties;
 
 	@Override
 	public void onUpdateReceived(Update update) {
-
+		TelegramUpdate telegramUpdate = telegramUpdateService.save(update);
+		telegramMessageHandlers.forEach(telegramMessageHandler -> telegramMessageHandler.handle(telegramUpdate));
 	}
 
 	@Override
 	public String getBotUsername() {
-
-		return null;
+		return xrpBotProperties.getUsername();
 	}
 
 	@Override
 	public String getBotToken() {
-		
-		return null;
+		return xrpBotProperties.getToken();
 	}
 }
