e5ad5166c5b74cff3e1c5639a7fbdd524a1ce440
Hat1ko <pasha1apple@gmail.com>
2020-03-28 14:34:40 +0200

    substitued old keboard generation by new in Bot class

--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/XrpLongPollingBot.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/XrpLongPollingBot.java
@@ -18,6 +18,7 @@ import com.hatiko.ripple.telegram.bot.core.dto.TelegramUpdate;
 import com.hatiko.ripple.telegram.bot.core.handler.TelegramMessageHandler;
 import com.hatiko.ripple.telegram.bot.core.properties.ActionProperties;
 import com.hatiko.ripple.telegram.bot.core.properties.XrpBotProperties;
+import com.hatiko.ripple.telegram.bot.core.service.KeyboardPreparator;
 import com.hatiko.ripple.telegram.bot.core.transformer.Transformer;
 
 import lombok.extern.slf4j.Slf4j;
@@ -30,18 +31,19 @@ public class XrpLongPollingBot extends TelegramLongPollingBot {
 
 	private final XrpBotProperties xrpBotProperties;
 	private final ActionProperties commandProperties;
+	private final KeyboardPreparator keyboardPreparator;
 	private final Transformer<Update, TelegramUpdate> updateToTelegramUpdateTransformer;
 
-	// instead of requiredArgcConstructor as cycling so obliged to use lazy
-	// invokation
 	@Autowired
 	public XrpLongPollingBot(@Lazy List<TelegramMessageHandler> telegramMessageHandlers,
 			XrpBotProperties xrpBotProperties, ActionProperties commandProperties,
+			KeyboardPreparator keyboardPreparator,
 			Transformer<Update, TelegramUpdate> updateToTelegramUpdateTransformer) {
 
 		this.telegramMessageHandlers = telegramMessageHandlers;
 		this.xrpBotProperties = xrpBotProperties;
 		this.commandProperties = commandProperties;
+		this.keyboardPreparator = keyboardPreparator;
 		this.updateToTelegramUpdateTransformer = updateToTelegramUpdateTransformer;
 	}
 
@@ -70,38 +72,12 @@ public class XrpLongPollingBot extends TelegramLongPollingBot {
 		sendMessage.setChatId(chatId);
 		sendMessage.setText(text);
 
-		sendMessage.setReplyMarkup(getCustomReplyKeyboardMarkup());
+		sendMessage.setReplyMarkup(keyboardPreparator.getMainKeyboard());
 
 		try {
 			execute(sendMessage);
 		} catch (TelegramApiException e) {
 			log.error("TelegramApiException : {}", e.getMessage());
 		}
-	}
-
-	private ReplyKeyboardMarkup getCustomReplyKeyboardMarkup() {
-
-		ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-		keyboardMarkup.setSelective(Boolean.TRUE);
-		keyboardMarkup.setResizeKeyboard(Boolean.TRUE);
-		keyboardMarkup.setOneTimeKeyboard(Boolean.FALSE);
-
-		List<KeyboardRow> keyboard = new ArrayList<>();
-
-		KeyboardRow firstKeyboardRow = new KeyboardRow();
-		KeyboardButton HelloButton = new KeyboardButton(commandProperties.getCommand().getHello());
-		HelloButton.setText(commandProperties.getButton().getHello());
-		firstKeyboardRow.add(HelloButton);
-
-		KeyboardRow secondKeyboardRow = new KeyboardRow();
-		KeyboardButton HelpButton = new KeyboardButton(commandProperties.getCommand().getHelp());
-		HelpButton.setText(commandProperties.getButton().getHelp());
-		secondKeyboardRow.add(HelpButton);
-
-		keyboard.add(firstKeyboardRow);
-		keyboard.add(secondKeyboardRow);
-		keyboardMarkup.setKeyboard(keyboard);
-
-		return keyboardMarkup;
 	}
 }
