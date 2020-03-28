29e6ca4bfaa935338f50c032622e6c3e7c7a48f9
Hat1ko <pasha1apple@gmail.com>
2020-03-28 14:29:01 +0200

    added two missing skeletons of handlers

---
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/handler/navigation/StartMessageHandler.java
@@ -0,0 +1,14 @@
+package com.hatiko.ripple.telegram.bot.core.handler.navigation;
+
+import com.hatiko.ripple.telegram.bot.core.dto.TelegramUpdate;
+import com.hatiko.ripple.telegram.bot.core.handler.TelegramMessageHandler;
+
+public class StartMessageHandler implements TelegramMessageHandler {
+
+	@Override
+	public void handle(TelegramUpdate telegramUpdate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/handler/operations/RegisterMessageHandler.java
@@ -0,0 +1,14 @@
+package com.hatiko.ripple.telegram.bot.core.handler.operations;
+
+import com.hatiko.ripple.telegram.bot.core.dto.TelegramUpdate;
+import com.hatiko.ripple.telegram.bot.core.handler.TelegramMessageHandler;
+
+public class RegisterMessageHandler implements TelegramMessageHandler {
+
+	@Override
+	public void handle(TelegramUpdate telegramUpdate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
