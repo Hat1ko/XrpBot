b58654b577f27ec7c5c59ce481ec56808f156d6d
Hat1ko <pasha1apple@gmail.com>
2020-03-29 14:58:28 +0300

    implemented one more button

--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/handler/operations/GetTransactionInfoMessageHandler.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/handler/operations/GetTransactionInfoMessageHandler.java
@@ -1,5 +1,7 @@
 package com.hatiko.ripple.telegram.bot.core.handler.operations;
 
+import java.lang.reflect.Method;
+
 import org.springframework.stereotype.Component;
 
 import com.hatiko.ripple.telegram.bot.core.XrpLongPollingBot;
@@ -8,8 +10,7 @@ import com.hatiko.ripple.telegram.bot.core.handler.TelegramMessageHandler;
 import com.hatiko.ripple.telegram.bot.core.properties.ActionProperties;
 import com.hatiko.ripple.telegram.bot.core.service.KeyboardPreparator;
 import com.hatiko.ripple.telegram.bot.core.service.LongTermOperationService;
-import com.hatiko.ripple.wrapper.web.model.BalanceResponse;
-import com.hatiko.ripple.wrapper.web.model.TransactionResponse;
+import com.hatiko.ripple.wrapper.service.RippleService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,35 +18,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class AutoReplyMessageHandler implements TelegramMessageHandler {
+public class GetTransactionInfoMessageHandler implements TelegramMessageHandler {
 
 	private final XrpLongPollingBot xrpLongPollingBot;
-	private final ActionProperties actionProperties;
+	private final ActionProperties actionProperties; 
 	private final KeyboardPreparator keyboardPreparator;
 	private final LongTermOperationService operationService;
-
+	private final RippleService rippleService;
+	
 	@Override
 	public void handle(TelegramUpdate telegramUpdate) {
 
-		if (actionProperties.getCommands().stream()
-				.filter(e -> telegramUpdate.getMessage().getText().startsWith(e)).count() > 0) {
+		if(!telegramUpdate.getMessage().getText().startsWith(actionProperties.getButton().getGetTransactionInfo())) {
 			return;
 		}
 		
+		
 		Long chatId = telegramUpdate.getMessage().getChat().getId();
 		Integer messageId = telegramUpdate.getMessage().getId();
-		String argv = telegramUpdate.getMessage().getText();
+				
+		String operation = "getTransactionInfo";
 		
-		Object response = operationService.insertArgument(argv, (int)(long) chatId);
-		
-		String responseMessage = null;
-		if(response instanceof BalanceResponse) {
-			responseMessage = String.format("Your balance is %s", ((BalanceResponse) response).getAmount());
+		Method method;
+		try {
+			method = RippleService.class.getDeclaredMethod("getTransactionByHash", String.class);
+			operationService.addOpearion(chatId, messageId, operation, rippleService, method, 1);
+		} catch (NoSuchMethodException e) {
+			log.error(e.getMessage());
+		} catch (SecurityException e) {
+			log.error(e.getMessage());
 		}
-		if(response instanceof TransactionResponse) {
-			responseMessage = String.format("Sum of transaction is %s", ((TransactionResponse) response).getAmount());
-		}
 		
-		xrpLongPollingBot.sendMessage(chatId, responseMessage, keyboardPreparator.getMainKeyboard());
+		String text = "Insert your transaction hash";
+		
+		Integer sentMessageId = xrpLongPollingBot.sendMessage(chatId, text, null);
+		
 	}
 }
