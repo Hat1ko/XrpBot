5ebc365053a3e8345a195dc0352730071f89b98f
Hat1ko <pasha1apple@gmail.com>
2020-03-31 16:06:29 +0300

    created session skeleton methods

--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/service/SessionService.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/service/SessionService.java
@@ -1,5 +1,11 @@
 package com.hatiko.ripple.telegram.bot.service;
 
-public interface SessionService {
+import com.hatiko.ripple.telegram.bot.dto.session.ChatSession;
 
+public interface SessionService {
+	
+	void createSession(Long chatId, String username);
+	ChatSession getSession(Long chatId);
+	void deleteSession(Long chatId);
+	void logOutSessions();//scheduled
 }
--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/service/SessionServiceImpl.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/service/SessionServiceImpl.java
@@ -2,6 +2,8 @@ package com.hatiko.ripple.telegram.bot.service;
 
 import org.springframework.stereotype.Component;
 
+import com.hatiko.ripple.telegram.bot.dto.session.ChatSession;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,6 +11,30 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Component
 public class SessionServiceImpl implements SessionService {
-
 	
+	List<ChatSession> sessions = new ArrayList<>();
+	
+	@Override
+	public void createSession(Long chatId, String username) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ChatSession getSession(Long chatId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteSession(Long chatId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logOutSessions() {
+		// TODO Auto-generated method stub
+		
+	}
 }
