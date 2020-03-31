a5cbd7b4a27ae1289ed40ee41349fe6ee165500e
Hat1ko <pasha1apple@gmail.com>
2020-03-31 13:49:48 +0300

    added list preparation logic

--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/properties/MessageResponseProperties.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/core/properties/MessageResponseProperties.java
@@ -1,5 +1,11 @@
 package com.hatiko.ripple.telegram.bot.core.properties;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +24,11 @@ import lombok.Setter;
 @ConfigurationProperties("telegram.bot.message.response")
 public class MessageResponseProperties {
 
+	private List<String> getBalanceList;
+	private List<String> getTransactionInfoList;
+	private List<String> getLastTransactionsList;	
+	private List<String> withdrawList;
+	
 	private String start;
 	private String hello;
 	private String next;
@@ -31,4 +42,31 @@ public class MessageResponseProperties {
 	private String getLastTransactions;
 	private String withdraw;
 	private String error;
+	
+	private String separator; 
+
+	@PostConstruct
+	private void prepareLists() {
+		
+		getBalanceList = Arrays.asList(getBalance.split(separator));
+		getTransactionInfoList = Arrays.asList(getTransactionInfo.split(separator));
+		getLastTransactionsList = Arrays.asList(getLastTransactions.split(separator));
+		withdrawList = Arrays.asList(withdraw.split(separator));
+	}
+	
+	public List<String> getGetBalance(){
+		return getBalanceList;
+	}
+	
+	public List<String> getGetTransactionInfo(){
+		return getTransactionInfoList;
+	}
+	
+	public List<String> getGetLastTransactions(){
+		return getLastTransactionsList;
+	}
+	
+	public List<String> getWithdraw(){
+		return withdrawList;
+	}
 }
