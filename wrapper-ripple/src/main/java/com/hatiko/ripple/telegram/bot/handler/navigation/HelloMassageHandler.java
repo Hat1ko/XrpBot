354480575845de6f3c9eeb616e6bece2cc352fff
Hat1ko <pasha1apple@gmail.com>
2020-04-02 15:06:01 +0300

    removal of session on hello or /start

--- wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/handler/navigation/HelloMassageHandler.java
+++ wrapper-ripple/src/main/java/com/hatiko/ripple/telegram/bot/handler/navigation/HelloMassageHandler.java
@@ -2,13 +2,12 @@ package com.hatiko.ripple.telegram.bot.handler.navigation;
 
 import org.springframework.stereotype.Component;
 
-import com.hatiko.ripple.telegram.bot.XrpLongPollingBot;
 import com.hatiko.ripple.telegram.bot.database.service.XrpDatabaseOperator;
 import com.hatiko.ripple.telegram.bot.dto.telegram.TelegramUpdate;
 import com.hatiko.ripple.telegram.bot.handler.TelegramMessageHandler;
 import com.hatiko.ripple.telegram.bot.properties.ActionProperties;
-import com.hatiko.ripple.telegram.bot.service.KeyboardPreparator;
 import com.hatiko.ripple.telegram.bot.service.ResponseMessageOperator;
+import com.hatiko.ripple.telegram.bot.service.SessionService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +20,7 @@ public class HelloMassageHandler implements TelegramMessageHandler {
 	private final ActionProperties actionProperties;
 	private final ResponseMessageOperator responseMessageOperator;
 	private final XrpDatabaseOperator databaseOperator; 
+	private final SessionService sessionService;
 	
 	@Override
 	public void handle(TelegramUpdate telegramUpdate) {
@@ -30,6 +30,9 @@ public class HelloMassageHandler implements TelegramMessageHandler {
 		}
 
 		Long chatId = telegramUpdate.getMessage().getChat().getId();
+
+		sessionService.deleteSession(chatId);
+		
 		Integer sentMessageId = responseMessageOperator.responseHello(telegramUpdate.getMessage().getFrom().getFirstName(),
 				chatId);
 		databaseOperator.updateMessageId(chatId, sentMessageId, null);
